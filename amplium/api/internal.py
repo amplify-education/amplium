95c0576c8a20b3204667f5610cb041496dbb87be
Ron Levit <rlevit@amplify.com>
2020-10-01 15:30:22 -0400

    Return 200 all the time

--- amplium/api/internal.py
+++ amplium/api/internal.py
@@ -9,6 +9,6 @@ logger = logging.getLogger(__name__)
 def get_status():
     """Handler for the status path"""
     data = GRID_HANDLER.get_grid_info()
-    data_packet = {"status": "OK", "nodes": data}
-    status_code = 200 if data else 503
+    data_packet = {"status": "OK"}
+    status_code = 200
     return data_packet, status_code
