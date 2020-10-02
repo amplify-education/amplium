a709cffbb5fe3684e885345fc01e85854aca074d
Alex Oskotsky <aoskotsky@amplify.com>
2020-10-02 03:19:08 -0400

    fix lint errors

--- amplium/api/exceptions.py
+++ amplium/api/exceptions.py
@@ -4,7 +4,7 @@
 class AmpliumException(RuntimeError):
     """Thrown if there is no available capacity for new sessions"""
     def __init__(self, message, error, status_code):
-        super(AmpliumException, self).__init__(message)
+        super().__init__(message)
 
         self.error = error
         self.status_code = status_code
@@ -13,16 +13,16 @@ class AmpliumException(RuntimeError):
 class NoAvailableCapacityException(AmpliumException):
     """Thrown if there is no available capacity for new sessions"""
     def __init__(self, message=""):
-        super(NoAvailableCapacityException, self).__init__(message, "AMPLIUM_NO_AVAILABLE_CAPACITY", 429)
+        super().__init__(message, "AMPLIUM_NO_AVAILABLE_CAPACITY", 429)
 
 
 class NoAvailableGridsException(AmpliumException):
     """Thrown if there are no grids registered to Amplium"""
     def __init__(self, message=""):
-        super(NoAvailableGridsException, self).__init__(message, "AMPLIUM_NO_AVAILABLE_GRIDS", 429)
+        super().__init__(message, "AMPLIUM_NO_AVAILABLE_GRIDS", 429)
 
 
 class IntegrationNotConfigured(AmpliumException):
     """Thrown if we try to use an integration and it is not configured correctly"""
     def __init__(self, message=""):
-        super(IntegrationNotConfigured, self).__init__(message, "AMPLIUM_INTEGRATION_NOT_CONFIGURED", 500)
+        super().__init__(message, "AMPLIUM_INTEGRATION_NOT_CONFIGURED", 500)
